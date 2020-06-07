7a3d7a8d3a5e354acc0660506a6acf9c51e6fc33
root <root@localhost.localdomain>
2020-06-07 15:59:07 +0800

    实现检测温度和cpu占用并发送给客户端

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-var os = require('os-utils');  // 请求os包
-os.cpuUsage(function(v){
-		console.log( 'CPU Usage (%): ' + v );
-});
+var cpuLoad = 0, cpuT = 0;
+var message = "";
+var sysInfo = require("systeminformation");
+var net = require("net");
 
-os.cpuFree(function(v){
-		console.log( 'CPU Free:' + v );
+var server = net.createServer(function (socket) {
+    console.log("服务器创建完成");
+    // 当客户端发送数据时触发
+    socket.on("data", function (data) {
+        // 更新message字符串
+        if (data == "get") {
+            console.log("请求获取数据");
+            socket.write(message);//  向客户端发送最新数据
+        }
+
+    });
+
 });
+server.listen(558, (socket) => {
+    console.log(`服务器启动成功,端口号:${server.address().port}`);
+})
+function getInfo() {
+    sysInfo.networkInterfaces().then(data => console.log(data[1].ip4))
+    // 获取cpu主要温度
+    sysInfo.cpuTemperature().then(data => cpuT = Math.round(data.main));
+    // 获取系统所有负载信息
+    sysInfo.currentLoad().then(data => cpuLoad = Math.round(data.currentload));
+    message = `?${cpuT}-${cpuLoad}!`;
+}
+setInterval(function () {
+    getInfo();
+}, 2000);
