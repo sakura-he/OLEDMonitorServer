7e8fe6e5f2620c9210618a038a51f1576abb1386
root <root@localhost.localdomain>
2020-06-11 20:18:03 +0800

    修改tcp数据发送格式,扩展性更高

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
 var sysInfo = require("systeminformation");
 var net = require("net");
-var cpuLoad = 0, cpuT = 0, memUsed = 0, memFree = 0;
-var message = "";
-var infoKey = {
+var infoKey, infoValue;
+var message = "0@Err:Err-0@Err:Err-0@Err:Err-0@Err:Err";
+// 获取指定系统信息;具体配置可以参考 https://systeminformation.io/
+infoKey = {
     cpuCurrentspeed: 'avg',  // cpu当前平均速度 单位GHZ
     cpuTemperature: 'main',  // cpu当前平均温度 单位摄氏度
     mem: 'used,free',  // 已用内存和剩余内存 单位字节
     fsSize: '*'
-
 }
-var infoValue;
+
 var server = net.createServer(function (socket) {
     console.log("检测到TCP链接");
     // 当客户端发送数据时触发
     socket.on("data", function (data) {
-        // 更新message字符串
         if (data == "get") {
-            //console.log("请求获取数据");
+            console.log(message);
             socket.write(message);//  向客户端发送最新数据
         }
-
     });
-
 });
-server.listen(553, (socket) => {
-    console.log(`服务器启动成功${server.address().address},端口号:${server.address().port}`);
-})
-
+server.listen(553, (socket) => console.log(`服务器启动成功,端口号:${server.address().port}`))
+// 2s更新一次数据
 setInterval(function () {
     getInfo();
 }, 2000);
 
 // 获取服务器的系统信息
 function getInfo() {
-
     try {
         sysInfo.get(infoKey).then(data => infoValue = data);
         message =
-            `Cpu:${infoValue.cpuCurrentspeed.avg}GHz
-            -CpuT:${infoValue.cpuTemperature.main}℃
-            -MEMU:${Math.round(infoValue.mem.used / 100000000) / 10}GB
-            -MEMF:${Math.round(infoValue.mem.free / 100000000) / 10}GB`;
+        // 视图@icon@key@value@unit@all
+            `?1@1@Cpu@${infoValue.cpuCurrentspeed.avg}@GHz@1.9-1@8@CpuT@${infoValue.cpuTemperature.main}@C@100-1@3@MEM@${Math.round(infoValue.mem.used / 100000000) / 10}/${Math.round(infoValue.mem.free / 100000000) / 10}@GB@0!`;
     } catch (error) {
-        message = "Err:Err-Err:Err-Err:Err-Err:Err"
+        console.log(error);
     }
-
 }
-
-
