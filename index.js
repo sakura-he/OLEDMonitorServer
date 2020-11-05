2b5897919781bb789c45ccf55224dc645f487678
sakura-he <hibikihe@gmail.com>
2020-11-06 00:54:54 +0800

    适配OledmonitorClient0.44

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-var sysInfo = require("systeminformation");
-var net = require("net");
-var infoKey, infoValue;
-var message = "0@Err:Err-0@Err:Err-0@Err:Err-0@Err:Err";
-// 获取指定系统信息;具体配置可以参考 https://systeminformation.io/
+var sysInfo = require('systeminformation')
+var net = require('net')
+var infoKey, infoValue
+var message = '0@Err:Err-0@Err:Err-0@Err:Err-0@Err:Err'
+    // 获取指定系统信息;具体配置可以参考 https://systeminformation.io/
 infoKey = {
-    cpuCurrentspeed: 'avg',  // cpu当前平均速度 单位GHZ
-    cpuTemperature: 'main',  // cpu当前平均温度 单位摄氏度
-    mem: 'used,free',  // 已用内存和剩余内存 单位字节
+    cpuCurrentspeed: 'avg', // cpu当前平均速度 单位GHZ
+    cpuTemperature: 'main', // cpu当前平均温度 单位摄氏度
+    mem: '*', //'active,free,total', // 已用内存和剩余内存 单位字节
     currentLoad: 'currentload',
-    fsSize: '*'
+    fsSize: '*',
+    dockerInfo: '*',
+    versions: '*',
+    osInfo: '*',
+    system: '*',
+    processes: '*',
+    cpu: '*',
 }
 
-var server = net.createServer(function (socket) {
-    console.log("检测到TCP链接");
-    // 当客户端发送数据时触发
-    socket.on("data", function (data) {
-        if (data == "get") {
-            console.log(message);
-            socket.write(message);//  向客户端发送最新数据
+var server = net.createServer(function(socket) {
+    console.log('检测到TCP链接')
+        // 当客户端发送数据时触发
+    socket.on('data', function(data) {
+        if (data == 'get') {
+            console.log(message)
+            socket.write(message) //  向客户端发送最新数据
         }
-    });
-});
-server.listen(553, (socket) => console.log(`服务器启动成功,端口号:${server.address().port}`))
-// 2s更新一次数据
-setInterval(function () {
-    getInfo();
-}, 2000);
+    })
+})
+server.listen(34453, (socket) => console.log(`服务器启动成功,端口号:${server.address().port}`))
+    // 2s更新一次数据
+setInterval(function() {
+    getInfo()
+}, 3000)
 
 // 获取服务器的系统信息
 function getInfo() {
-    try {
-        sysInfo.get(infoKey).then(data => infoValue = data);
-        message =
-            //传递格式: @是分隔符
-            //    视图模式(1大字模式,2列表模式)@图标(以遗弃)or列表模式的第二个信息@系统信息标题@系统信息值@单位@最大值
-            `?1@1@Cpu@${infoValue.cpuCurrentspeed.avg}@GHz@1.99
-            -1@9@CPUtemperature@${infoValue.cpuTemperature.main}@*C@100
-            -2@CPULoaded#${Math.round(infoValue.currentLoad.currentload)}#%#100@MEM@${Math.round(infoValue.mem.used / 100000000) / 10}@GB@4.2
-            !`;
-    } catch (error) {
-        console.log(error);
-    }
+    sysInfo
+        .get(infoKey)
+        .then((data) => {
+            infoValue = data
+            message = `?1@Cpu@${infoValue.cpuCurrentspeed.avg}@GHz@1.99@1\\1@CPUtemperature@${infoValue.cpuTemperature.main}@*C@100@0\\2@MEM@${Math.round(infoValue.mem.active / 100000000) / 10}@GB@${Math.round(infoValue.mem.total / 100000000) / 10}@CPULoaded#${Math.round(infoValue.currentLoad.currentload)}#%#100\\3@Docker:${infoValue.dockerInfo.containers}@value@unit@max@运行中#${infoValue.dockerInfo.containersRunning}#个#已暂停#${infoValue.dockerInfo.containersPaused}#个#已停止#${infoValue.dockerInfo.containersStopped}#个\\3@软件版本@value@unit@max@Nodejs#${infoValue.versions.node}# #Docker#${infoValue.versions.docker}# #python#${infoValue.versions.python}# !`
+            console.log(message)
+            console.log(infoValue)
+        })
+        .catch((_error) => console.log(_error))
 }
-//${Math.round(infoValue.mem.free / 100000000) / 10}
